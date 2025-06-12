8382b4e851370de3ef09e764067ea6103a9aa6b0
The Lucky Clover <28643016+the-lucky-clover@users.noreply.github.com>
2025-06-12 03:23:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const puppeteerRoute = require('./routes/puppeteerRoute');
 
 app.use(express.json());
-app.use('/api', puppeteerRoute);
+
+// Option 3: Add logging to see what requests are coming in
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+// Option 1: Change from '/api' to '/' for route registration
+app.use('/', puppeteerRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
